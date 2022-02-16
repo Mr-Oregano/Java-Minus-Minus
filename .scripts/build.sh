0280f4d52065038162906b519645a0449dfade07
Spice <kf.shared.ptr@gmail.com>
2022-02-16 15:58:50 -0600

    Fixed errors in build.sh.

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,14 +1,15 @@
 mkdir -p $DOMAIN/Transformation/bin
 
 # Build Lexer & Parser
+echo -e "\n===================================== BUILD PARSER =====================================\n"
 SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}") 
 sh $SCRIPT_RELATIVE_DIR/build-parser.sh $1 $2
 
 # Build: Transformer
-SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}") 
-sh SCRIPT_RELATIVE_DIR/build-transformer.sh $1 $2
+echo -e "\n===================================== BUILD TRANSFORMER =====================================\n"
+sh $SCRIPT_RELATIVE_DIR/build-transformer.sh $1 $2
 
 # Build: Pretty Printer
-SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}") 
-sh SCRIPT_RELATIVE_DIR/build-prettyprinter.sh
+echo -e "\n===================================== BUILD PRETTY PRINTER =====================================\n"
+sh $SCRIPT_RELATIVE_DIR/build-prettyprinter.sh
 
